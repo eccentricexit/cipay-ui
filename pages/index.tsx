dcec9a7eed80ec1b58e5691e46bce803371a95b0
mtsalenc <mtsalenc@gmail.com>
2020-12-30 01:23:04 -0300

    chore: ui scaffolding

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,44 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import {
+  Provider as BumbagProvider,
+  PageContent,
+  Input,
+  Button,
+  Stack,
+  Card,
+  Flex,
+} from 'bumbag'
+import { useCallback, useState } from 'react'
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const IndexPage = () => {
+  const [isLoading, setIsLoading] = useState<boolean | undefined>()
+
+  const onPayRequest = useCallback(() => {
+    setIsLoading(true)
+    // TODO: Request signature to send x DAI from
+    // TODO: Send QR Code + signature to backend.
+    setTimeout(() => setIsLoading(false), 3000)
+  }, [])
+
+  return (
+    <BumbagProvider>
+      <PageContent>
+        <Card>
+          <Stack>
+            <Input placeholder="Enter your the QR code here." />
+            <Flex alignX="right">
+              <Button
+                palette="primary"
+                isLoading={isLoading}
+                onClick={onPayRequest}
+              >
+                Pay
+              </Button>
+            </Flex>
+          </Stack>
+        </Card>
+      </PageContent>
+    </BumbagProvider>
+  )
+}
 
 export default IndexPage
